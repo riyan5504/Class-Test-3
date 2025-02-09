82dc0ea3b03fc63ec98610c4896a7b44ce52b71f
riyan5504 <ahsan.natore@gmail.com>
2025-02-09 22:55:36 +0600

    Class Test

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let age;
+
+age = 4;
+
+if(age>=60){
+    console.log("Ticket Price is $8")
+}
+
+else if(age>=18){
+    console.log("Ticket Price is $15")
+}
+
+else if(age>=5){
+    console.log("Ticket Price is $10")
+}
+
+else {
+    console.log("Ticket is Free")
+}
